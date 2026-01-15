c60fc831d9a120f6305e48785779c663e89ef1ae
Md. Nazmul Hasan <nazmul.hasan.se@gmail.com>
2026-01-15 13:02:45 +0600

    Update TopBanner.tsx

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -3,17 +3,5 @@ import { X } from 'lucide-react'
 import { useAppState } from '../store'
 
 export function TopBanner() {
-  const { isBannerVisible, setBannerVisible } = useAppState();
-
-  const handleCloseBanner = useCallback(() => {
-    setBannerVisible(false);
-  }, [setBannerVisible]);
-
-  if (!isBannerVisible) {
-    return null;
-  }
-
-  return (
-    
-  );
+ 
 }
